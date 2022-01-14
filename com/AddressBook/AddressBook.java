277621d54adcbe934687a4b5a4b763958a5bfab5
smrutikapatil <smruti.patil17@gmail.com>
2022-01-14 18:11:25 +0530

    Added- Created a contact in address book

--- com/AddressBook/AddressBook.java
+++ com/AddressBook/AddressBook.java
@@ -1,8 +1,33 @@
 package com.AddressBook;
 
+import java.util.Scanner;
+
 public class AddressBook {
 	public static void main(String[] args) {
 		System.out.println( "Welcome to Address Book" );
+		AddressBook createContact = new AddressBook();
+        System.out.println("Enter To Create Contact Details in Address Book  ");
+        createContact.createContact();
+    }
+    public void createContact(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter First Name: ");
+        String firstName = sc.nextLine();
+        System.out.println("Enter Last Name: ");
+        String lastName = sc.nextLine();
+        System.out.println("Enter Address: ");
+        String address = sc.nextLine();
+        System.out.println("Enter City Name: ");
+        String city = sc.nextLine();
+        System.out.println("Enter State Name: ");
+        String state = sc.nextLine();
+        System.out.println("Enter Zip Code: ");
+        int zipcode = sc.nextInt();
+        System.out.println("Enter Phone Number: ");
+        long phoneNumber = sc.nextLong();
+        System.out.println("Enter Email  Address : ");
+        String emailAddress = sc.next();
+        System.out.println( "Contact has been Created !!" );
 	}
 		
 }
